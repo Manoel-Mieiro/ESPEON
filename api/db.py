adf051978af0b160b7aeddff5544e1bb95426aa1
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-07-14 14:18:25 -0300

    refactor: streamline MongoDB collection management in seedMongo function

--- api/db.py
+++ api/db.py
@@ -1,40 +1,30 @@
 import os
+import json
 from dotenv import load_dotenv
 from pymongo import MongoClient
 
 load_dotenv(".env")
 
 MONGO_URI = os.getenv("MONGO_URI")
-DATABASE_NAME = os.getenv("MONGO_DB")
-COLLECTION = os.getenv("MONGO_COLLECTION")
-
-collections = [COLLECTION, "users", "login", "lectures"]
-
 client = MongoClient(MONGO_URI)
-database = client[DATABASE_NAME]
-collection = database[collections[0]]
-usersCollection = database[collections[1]]
-loginCollection = database[collections[2]]
-lecturesCollection = database[collections[3]]
-
-
-def showMongo():
-    return {
-        "MONGO": MONGO_URI,
-        "DATABASE": DATABASE_NAME,
-        "COLLECTIONS": collections
-    }
 
 
 def seedMongo():
-    existing_collections = database.list_collection_names()
+    with open("db.json", "r") as f:
+        data = json.load(f)
+    for db_item in data:
+        db_name = db_item["database"]
+        collection_names = db_item["collections"]
 
-    for c in collections:
-        if c in existing_collections:
-            print(f"Collection '{c}' já existe.")
-        else:
-            try:
-                database.create_collection(c)
-                print(f"Collection '{c}' criada com sucesso.")
-            except Exception as e:
-                print(f"Erro ao criar a collection '{c}': {e}")
+        db = client[db_name]
+        existing_collections = db.list_collection_names()
+
+        for c in collection_names:
+            if c in existing_collections:
+                print(f"[{db_name}] Collection '{c}' já existe.")
+            else:
+                try:
+                    db.create_collection(c)
+                    print(f"[{db_name}] Collection '{c}' criada com sucesso.")
+                except Exception as e:
+                    print(f"[{db_name}] Erro ao criar a collection '{c}': {e}")
