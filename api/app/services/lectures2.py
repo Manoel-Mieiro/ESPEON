db11f501b0edc42e19eee40d9157e34bee14de1d
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-07-16 19:59:55 -0300

    feat: enhance findAllLectures to standardize lecture names before returning

--- api/app/services/lectures2.py
+++ api/app/services/lectures2.py
@@ -1,9 +1,17 @@
 import app.repository.lecturesRepository2 as lectures
 
+
 def findAllLectures():
     try:
-        return lectures.findAllLectures()
+        lectureList = lectures.findAllLectures()
+        for i, l in enumerate(lectureList):
+            lectureList[i] = lectureStandardize(l)
+        return lectureList
     except Exception as e:
         print("[SERVICE]Error fetching lectures:", e)
         raise e
-    
+
+
+def lectureStandardize(lecture):
+    noUnderscore = lecture.replace("_", " ")
+    return noUnderscore.upper()
