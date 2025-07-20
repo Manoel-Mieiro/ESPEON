953bf0414f45f3637d4f8e7b4aa966c02db2cfc8
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-07-20 19:40:31 -0300

    fix: findAllTraces response now is set to return as json. Plus, append function inside extractField was fixed in order to reference extracedData list.

--- api/app/services/reports/raw.py
+++ api/app/services/reports/raw.py
@@ -8,7 +8,7 @@ def findAllTraces(subject):
         url = f"http://localhost:8183/traces/{subject}"
         response = requests.get(url)
         print(f"Response para {subject}: {response}")
-        return response
+        return response.json()
     except Exception as e:
         raise Exception(
             f"[SERVICE] An error occurred while attempting to get traces from '{subject}': {e}")
@@ -17,5 +17,5 @@ def extractField(data, field):
     extractedData = []
     for d in data:
         if field in d:
-            extractField.append(d[field])
+            extractedData.append(d[field])
     return extractedData
