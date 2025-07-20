130a3e92e7c5cfc142c8fb4c90dc32cf9d01b3df
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-07-20 18:24:45 -0300

    feat: added boolPercentage function in order to build donut charts

--- api/app/services/reports/metrics.py
+++ api/app/services/reports/metrics.py
@@ -8,42 +8,64 @@ def builReport(subject):
     except Exception as e:
         raise e
 
+
 def countDistinct(arr):
     distinct = set(arr)
     return len(distinct)
+
 
 def assembleData():
     countLecutre = countLecture()
     countStudents = countStudents()
     totalTimeWatched = totalTimeWatched()
-    avgLectureDuration = ""
-    avgIdleDuration = ""
-    avgAttentionSpan = ""
-    pctEnabledCamera = ""
-    pctEnabledMic = ""
-    avgCamStreamingSpan = ""
-    avgMicStreamingSpan = ""
+    avgLectureDuration = avgLectureDuration()
+    avgIdleDuration = avgIdleDuration()
+    avgAttentionSpan = avgAttentionSpan()
+    pctEnabledCamera = boolPercentage("cameraEnabled")
+    pctEnabledMic = boolPercentage("microphoneEnabled")
+    avgCamStreamingSpan = boolPercentage("cameraStreaming")
+    avgMicStreamingSpan = boolPercentage("microphoneStreaming")
     return {
         "countLecutre": countLecutre,
         "countStudents": countStudents,
         "totalTimeWatched": totalTimeWatched,
-        "avgLectureDuration": "self.countLecutre",
-        "avgIdleDuration": "self.countLecutre",
-        "avgAttentionSpan": "self.countLecutre",
-        "pctEnabledCamera": "self.countLecutre",
-        "pctEnabledMic": "self.countLecutre",
-        "avgCamStreamingSpan": "self.countLecutre",
-        "avgMicStreamingSpan": "self.countLecutre",
+        "avgLectureDuration": avgLectureDuration,
+        "avgIdleDuration": avgIdleDuration,
+        "avgAttentionSpan": avgAttentionSpan,
+        "pctEnabledCamera": pctEnabledCamera,
+        "pctEnabledMic": pctEnabledMic,
+        "avgCamStreamingSpan": avgCamStreamingSpan,
+        "avgMicStreamingSpan": avgMicStreamingSpan,
     }
+
 
 def countLecture():
     data = raw.extractField("classTitle")
     return countDistinct(data)
 
+
 def countStudents():
     data = raw.extractField("user")
     return countDistinct(data)
 
-def totalTimeWatched():
-    return 0 # por enquanto vou retornar 0
 
+def totalTimeWatched():
+    return 0  # por enquanto vou retornar 0
+
+
+def avgLectureDuration():
+    return 1  # por enquanto vou retornar 1
+
+
+def avgIdleDuration():
+    return 1  # por enquanto vou retornar 1
+
+
+def avgAttentionSpan():
+    return 0  # por enquanto vou retornar 0
+
+
+def boolPercentage(field):
+    data = raw.extractField(field)
+    occurences = data.count("yes")
+    return occurences/len(data)
