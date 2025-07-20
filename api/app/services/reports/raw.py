81c25372731d67fa5532e8c9b16b01942aa56f22
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-07-20 17:41:40 -0300

    feat: created raw data file in services layer

---
+++ api/app/services/reports/raw.py
@@ -0,0 +1,20 @@
+# pegar todos os logs de uma collection
+# transformar em dicionário
+import requests
+
+
+def findAllTraces(subject):
+    try:
+        url = f"http://localhost:8183/traces/{subject}"
+        response = requests.get(url)
+        return response
+    except Exception as e:
+        raise Exception(
+            f"[SERVICE] An error occurred while attempting to get traces from '{subject}': {e}")
+    
+def extractField(data, field):
+    extractedData = []
+    for d in data:
+        if field in d:
+            extractField.append(d[field])
+    return extractedData
