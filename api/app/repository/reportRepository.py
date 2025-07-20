d4b8a1ec006089c857f459f4cfbf5d8afb133dd6
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-07-20 16:43:56 -0300

    feat: created reportRepository in order to store reports based on kind inside REPORTS database

---
+++ api/app/repository/reportRepository.py
@@ -0,0 +1,38 @@
+from bson import ObjectId
+from db import client
+from app.models.reports import Reports
+
+db = client["REPORTS"]
+
+
+def findAllReports(kind):
+    reports = db[kind]
+    try:
+        docs = list(reports.find({}))
+        report_list = []
+
+        for doc in docs:
+            report = Reports.from_dict(doc)
+            report_list.append(report.to_dict())
+
+        return report_list
+
+    except Exception as e:
+        print("[REPOSITORY]Erro ao buscar reports:", e)
+        raise e
+
+
+def createReport(data: Reports, kind):
+    reports = db[kind]
+    try:
+        print("\n[REPOSITORY]Criando report:", data, "\n")
+        result = reports.insert_one(data.to_dict())
+        data._id = result.inserted_id
+
+        if not data._id:
+            raise Exception("Erro ao inserir report no banco.")
+
+        return data.to_dict()
+    except Exception as e:
+        print("[REPOSITORY]Erro ao criar report:", e)
+        raise e
