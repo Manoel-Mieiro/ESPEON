bd81e22dfadc877dace6b17c34028ca17230cb21
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-07-21 21:58:36 -0300

    feat: updating createReports args

--- api/app/controllers/reports.py
+++ api/app/controllers/reports.py
@@ -8,9 +8,9 @@ def listReports(kind):
         raise e
 
 
-def createReports(kind, subject):
+def createReports(kind, subject=None, lecture=None):
     try:
-        return reportService.createReport(kind, subject)
+        return reportService.createReport(kind, subject, lecture)
     except Exception as e:
         print("[CONTROLLER]Error creating report:", e)
         raise e
