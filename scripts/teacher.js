fdcdf2d3973db618655e553e455b943dc53edac9
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-09-28 14:25:53 -0300

    update: tronando método async para resgatar o user do storage e resolvê-lo no front

--- scripts/teacher.js
+++ scripts/teacher.js
@@ -1,6 +1,18 @@
 import { logout } from "./auth/logout.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", async () => {
   const exit = document.getElementById("exit_style");
+  const profile = document.getElementById("profile");
+
+  const result = await chrome.storage.session.get("user");
+
+  const username = result.user;
+
+  const alias = username ? username.split("@")[0] : "Dummy";
+
+  if (profile) {
+    profile.textContent = alias;
+  }
+
   logout(exit);
 });
