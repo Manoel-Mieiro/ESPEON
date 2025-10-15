77a697c07706b5ac5f32d2bfff5f7e78d975ca09
Manoel-Mieiro <mjlmieiro@gmail.com>
2025-10-15 14:40:11 -0300

    fix: removendo arquivos de lecture desnecessários

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,7 +3,6 @@ from app.routes.traces import traces_bp
 from app.routes.users import users_bp
 from app.routes.login import login_bp
 from app.routes.lectures import lectures_bp
-from app.routes.lectures2 import lectures2_bp
 from app.routes.subject import subjects_bp
 from app.routes.reports import report_bp
 from app.routes.reportsStudent import report_student_bp
@@ -15,7 +14,6 @@ def create_app():
     app.register_blueprint(users_bp)
     app.register_blueprint(login_bp)
     app.register_blueprint(lectures_bp)
-    app.register_blueprint(lectures2_bp)
     app.register_blueprint(subjects_bp)
     app.register_blueprint(report_bp)
     app.register_blueprint(report_student_bp)
